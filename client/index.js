e18e5ea86fc61474c0b6a8e90f5ae7b3a27a107b
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-28 12:27:08 -0600

    did not complete assessment as I would have liked

--- client/index.js
+++ client/index.js
@@ -1,5 +1,9 @@
 const fortuneBtn = document.getElementById("fortuneBtn");
 const quotesBtn = document.getElementById("quotes");
+const divEl = document.querySelector("div");
+const removeBtn = document.getElementById("removeQuote");
+
+const baseURL = "http://localhost:4000/api";
 
 fortuneBtn.addEventListener("click", () => {
   axios.get("http://localhost:4000/api/fortune").then(function (response) {
@@ -10,7 +14,65 @@ fortuneBtn.addEventListener("click", () => {
 
 quotesBtn.addEventListener("click", () => {
   axios.get("http://localhost:4000/api/quotes").then(function (response) {
-    const data = response.data;
-    alert(data);
+    const h2 = document.createElement("h2");
+    const text = document.createTextNode(`${response.data}`);
+    h2.appendChild(text);
+    divEl.appendChild(h2);
   });
 });
+function clearQuotes() {
+  divEl.innerHTML = "";
+}
+removeBtn.addEventListener("click", clearQuotes);
+
+// submit buttons
+const getSubmit = document.getElementById("getSubmit");
+const getParamsSubmit = document.getElementById("getParamsSubmit");
+const getQuerySubmit = document.getElementById("getQuerySubmit");
+
+// inputs
+const paramsInput = document.getElementById("params-input");
+const queryInput = document.getElementById("query-input");
+
+// response section
+const responseSection = document.getElementsByClassName("response-area")[0];
+
+// handle submits
+getSubmit.addEventListener("click", () => {
+  axios
+    .get("http://localhost:4000/api/inventory")
+    .then((res) => addToView(res.data));
+});
+
+getParamsSubmit.addEventListener("click", () => {
+  axios
+    .get(`http://localhost:4000/api/inventory/${paramsInput.value}`)
+    .then((res) => addToView([res.data]));
+});
+
+getQuerySubmit.addEventListener("click", () => {
+  axios
+    .get(`http://localhost:4000/api/inventory?item=${queryInput.value}`)
+    .then((res) => addToView(res.data));
+});
+
+// handle response
+function addToView(dataArr) {
+  responseSection.innerHTML = null;
+
+  if (dataArr.length === 0) {
+    const p = document.createElement("p");
+    const t = document.createTextNode("Response came back with no results!");
+    p.appendChild(t);
+
+    responseSection.appendChild(p);
+  } else {
+    dataArr.forEach((item) => {
+      const p = document.createElement("p");
+      const t = document.createTextNode(item);
+      p.appendChild(t);
+
+      responseSection.appendChild(p);
+    });
+  }
+}
--- server/index.js
+++ server/index.js
@@ -7,6 +7,15 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+const quotes = [
+  `I don't walk away from things I think are unfinished. - Arnold Schwarzenegger`,
+  "The mind is everything. What you think you become. - Buddha",
+  `Life shrinks or expands in proportion to one's courage. - Anais Nin`,
+  `It is never too late to be what you might have been. - George Eliot`,
+  `Too many of us are not living our dreams because we are living our fears. - Les Brown`,
+  `I didn't fail the test. I just found 100 ways to do it wrong. - Benjamin Franklin`,
+];
+
 app.get("/api/compliment", (req, res) => {
   const compliments = [
     "Gee, you're a smart cookie!",
@@ -35,16 +44,44 @@ app.get("/api/fortune", (req, res) => {
 });
 
 app.get("/api/quotes", (req, res) => {
-  const quotes = [
-    `I don't walk away from things I think are unfinished. - Arnold Schwarzenegger`,
-    "The mind is everything. What you think you become. - Buddha",
-    `Life shrinks or expands in proportion to one's courage. - Anais Nin`,
-    `It is never too late to be what you might have been. - George Eliot`,
-  ];
   let randomIndex = Math.floor(Math.random() * quotes.length);
   let randomQuote = quotes[randomIndex];
 
   res.status(200).send(randomQuote);
 });
 
-app.listen(4000, () => console.log("Server running on 4000"));
+const mockDb = [
+  "pencil",
+  "pen",
+  "sticker",
+  "dirt",
+  "leaf",
+  "rock",
+  "hippopotamus",
+  "spiders",
+  "mouse",
+  "soccerball",
+];
+
+app.get("/api/inventory", (req, res) => {
+  const { item } = req.query;
+
+  if (item) {
+    const filteredItems = mockDb.filter((el) => {
+      return el.includes(item);
+    });
+    res.status(200).send(filteredItems);
+  } else {
+    console.log("hit");
+    res.status(200).send(mockDb);
+  }
+});
+
+app.get("/api/inventory/:itemInd", (req, res) => {
+  res.status(200).send(mockDb[+req.params.itemInd]);
+});
+
+app.get("/api/inventory", (req, res) => {});
+
+app.listen(4000, () => console.log("Server running on port 4000"));
+app.listen(4000, () => console.log("Server running on port 4000"));
